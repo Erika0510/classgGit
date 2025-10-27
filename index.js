c82f7b1a9091d3b6c83f5683070d526d282a558d
Erika <etbr510@gmail.com>
2025-10-27 12:42:50 -0500

    t

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000 || process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`hola mundo en el puerto `);
+});
